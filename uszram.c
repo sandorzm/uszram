70b5202f1879ccacc365a8afa2c4b5be8cfc71cd
Sandor Magyar <sandorzm@gmail.com>
2021-03-06 14:30:42 -0500

    Fix huge page support; make addresses correctly use unsigned type

--- uszram.c
+++ uszram.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <threads.h>
 
-#include "lz4.h"
+#include <lz4.h>
 
 #include "uszram.h"
 
@@ -16,8 +16,8 @@
  * used for memory efficiency so we want to keep memory footprint small so we
  * can squeeze size and flags into a field."
  */
-#define FLAG_SHIFT  28
-#define GET_SIZE(pg) (pg->flags & ((1 << FLAG_SHIFT) - 1))
+#define FLAG_SHIFT 28
+#define GET_SIZE(pg) (pg->flags & (((uint_least32_t)1 << FLAG_SHIFT) - 1))
 
 // Flags for uszram pages (pgtbl[page_no].flags)
 enum uszram_pgflags {
@@ -27,37 +27,25 @@ enum uszram_pgflags {
 
 struct uszram_page {
 	char *data;
-	int_least32_t flags;
+	uint_least32_t flags;
 };
 
 struct uszram_stats {
-	unsigned long long compr_data_size;	// compressed size of pages stored
-	unsigned long long num_reads;		// failed + successful
-	unsigned long long num_writes;		// --do--
-	unsigned long long failed_reads;	// can happen when memory is too low
-	unsigned long long failed_writes;	// can happen when memory is too low
-	unsigned long long invalid_io;		// non-page-aligned I/O requests
-	unsigned long long notify_free;		// no. of swap slot free notifications
-	unsigned long long huge_pages;		// no. of huge pages
-	unsigned long long huge_pages_since;	// no. of huge pages since zram set up
-	unsigned long long pages_stored;	// no. of pages currently stored
-	unsigned long long max_used_pages;	// no. of maximum pages stored
-	unsigned long long writestall;		// no. of write slow paths
-	unsigned long long miss_free;		// no. of missed free
+	uint_least64_t compr_data_size;		// compressed size of pages stored
+	uint_least64_t num_reads;		// failed + successful
+	uint_least64_t num_writes;		// --do--
+	uint_least64_t failed_reads;		// can happen when memory is too low
+	uint_least64_t failed_writes;		// can happen when memory is too low
+	uint_least64_t huge_pages;		// no. of huge pages
+	uint_least64_t huge_pages_since;	// no. of huge pages since zram set up
+	uint_least64_t pages_stored;		// no. of pages currently stored
+	uint_least64_t max_used_pages;		// no. of maximum pages stored
 };
 
 static struct uszram {
 	struct uszram_page pgtbl[PG_ADDR_MAX + 1];
 	mtx_t locks[LOCK_COUNT];
 	struct uszram_stats stats;
-
-	/* This is the limit on amount of *uncompressed* worth of data we can
-	 * store in a disk.
-	 */
-	/* unsigned long long disksize;	// bytes */
-	/* char compressor[CRYPTO_MAX_ALG_NAME]; */
-	// zram is claimed so open request will be failed
-	/* _Bool claim; // Protected by bdev->bd_mutex */
 } uszram;
 
 int uszram_init(void)
@@ -65,46 +53,74 @@ int uszram_init(void)
 	return 0;
 }
 
-int uszram_read_blk(int_least32_t blk_addr, char data[static USZRAM_BLOCK_SIZE])
+int uszram_read_blk(uint_least32_t blk_addr, char data[static USZRAM_BLOCK_SIZE])
 {
 	if (blk_addr > USZRAM_BLK_ADDR_MAX)
 		return -1;
 
-	int_least32_t pg_addr = blk_addr / BLK_PER_PG;
-	unsigned long lock_addr = pg_addr / USZRAM_PG_PER_LOCK;
-	unsigned long byte_offset
-		= blk_addr % BLK_PER_PG * USZRAM_BLOCK_SIZE;
+	uint_least32_t pg_addr = blk_addr / BLK_PER_PG;
 	struct uszram_page *pg = uszram.pgtbl + pg_addr;
-	char pg_buf[USZRAM_PAGE_SIZE];
+
+	// TODO Lock needed? Should uszram_page fields be _Atomic?
+	if (!pg->data) {
+		memset(data, 0, USZRAM_BLOCK_SIZE);
+		return 0;
+	}
+
+	uint_least32_t lock_addr = pg_addr / USZRAM_PG_PER_LOCK;
+	int byte_offset = blk_addr % BLK_PER_PG * USZRAM_BLOCK_SIZE;
 
 	// Lock uszram.locks[lock_addr] as reader
-	int ret = LZ4_decompress_safe_partial(pg->data, pg_buf, GET_SIZE(pg),
-					      byte_offset + USZRAM_BLOCK_SIZE,
-					      USZRAM_PAGE_SIZE);
-	// Unlock uszram.locks[lock_addr] as reader
-	ret -= byte_offset;
-	if (ret < 0)
-		return ret;
-	memcpy(data, pg_buf + byte_offset, USZRAM_BLOCK_SIZE);
-	return ret;
+	if (pg->flags & ((uint_least32_t)1 << HUGE_PAGE)) {
+		memcpy(data, pg->data + byte_offset, USZRAM_BLOCK_SIZE);
+		// Unlock uszram.locks[lock_addr] as reader
+	} else {
+		char pg_buf[USZRAM_PAGE_SIZE];
+		int ret = LZ4_decompress_safe_partial(
+			pg->data,
+			pg_buf,
+			GET_SIZE(pg),
+			byte_offset + USZRAM_BLOCK_SIZE,
+			USZRAM_PAGE_SIZE
+		);
+		// Unlock uszram.locks[lock_addr] as reader
+		if (ret < 0)
+			return ret;
+		memcpy(data, pg_buf + byte_offset, USZRAM_BLOCK_SIZE);
+	}
+	return 0;
 }
 
-int uszram_read_pg(int_least32_t pg_addr, char data[static USZRAM_PAGE_SIZE])
+int uszram_read_pg(uint_least32_t pg_addr, char data[static USZRAM_PAGE_SIZE])
 {
 	if (pg_addr > PG_ADDR_MAX)
 		return -1;
 
-	unsigned long lock_addr = pg_addr / USZRAM_PG_PER_LOCK;
 	struct uszram_page *pg = uszram.pgtbl + pg_addr;
 
+	// TODO Lock needed? Should uszram_page fields be _Atomic?
+	if (!pg->data) {
+		memset(data, 0, USZRAM_PAGE_SIZE);
+		return 0;
+	}
+
+	unsigned long lock_addr = pg_addr / USZRAM_PG_PER_LOCK;
+
 	// Lock uszram.locks[lock_addr] as reader
-	int ret = LZ4_decompress_safe(pg->data, data, GET_SIZE(pg),
-				      USZRAM_PAGE_SIZE);
-	// Unlock uszram.locks[lock_addr] as reader
-	return ret;
+	if (pg->flags & ((uint_least32_t)1 << HUGE_PAGE)) {
+		memcpy(data, pg->data, USZRAM_PAGE_SIZE);
+		// Unlock uszram.locks[lock_addr] as reader
+	} else {
+		int ret = LZ4_decompress_safe(pg->data, data, GET_SIZE(pg),
+					      USZRAM_PAGE_SIZE);
+		// Unlock uszram.locks[lock_addr] as reader
+		if (ret < 0)
+			return ret;
+	}
+	return 0;
 }
 
-int uszram_write_blk(int_least32_t blk_addr,
+int uszram_write_blk(uint_least32_t blk_addr,
 		     const char data[static USZRAM_BLOCK_SIZE])
 {
 	if (blk_addr > USZRAM_BLK_ADDR_MAX)
@@ -115,41 +131,40 @@ int uszram_write_blk(int_least32_t blk_addr,
 		= blk_addr % BLK_PER_PG * USZRAM_BLOCK_SIZE;
 	char pg_buf[USZRAM_PAGE_SIZE];
 
-	int read = uszram_read_pg(pg_addr, pg_buf);
-	if (read < 0)
-		return read;
+	int ret = uszram_read_pg(pg_addr, pg_buf);
+	if (ret < 0)
+		return ret;
 	memcpy(pg_buf + byte_offset, data, USZRAM_BLOCK_SIZE);
 	return uszram_write_pg(pg_addr, pg_buf);
 }
 
-int uszram_write_pg(int_least32_t pg_addr,
+int uszram_write_pg(uint_least32_t pg_addr,
 		    const char data[static USZRAM_PAGE_SIZE])
 {
 	if (pg_addr > PG_ADDR_MAX)
 		return -1;
 
-	unsigned long lock_addr = pg_addr / USZRAM_PG_PER_LOCK;
 	struct uszram_page *pg = uszram.pgtbl + pg_addr;
-	_Bool present = pg->data, huge = 0;
 	int size = GET_SIZE(pg);
+	unsigned long lock_addr = pg_addr / USZRAM_PG_PER_LOCK;
 	char pg_buf[USZRAM_PAGE_SIZE];
+	const char *data_src = pg_buf;
 
 	int new_size = LZ4_compress_default(data, pg_buf, USZRAM_PAGE_SIZE,
 					    USZRAM_PAGE_SIZE);
 	// Lock uszram.locks[lock_addr] as writer
-	if (new_size == 0) {
-		new_size = USZRAM_PAGE_SIZE;
-		pg->flags |= 1 << HUGE_PAGE;
-		huge = 1;
-	} else {
+	if (new_size) {
 		pg->flags = new_size;
+	} else {
+		pg->flags |= (uint_least32_t)1 << HUGE_PAGE;
+		new_size = USZRAM_PAGE_SIZE;
+		data_src = data;
 	}
-	if (!present || new_size != size) {
-		if (present)
-			free(pg->data);
-		pg->data = malloc(new_size);
+	if (!(pg->data && new_size == size)) {
+		free(pg->data);
+		pg->data = calloc(new_size, 1);
 	}
-	memcpy(pg->data, huge ? data : pg_buf, new_size);
+	memcpy(pg->data, data_src, new_size);
 	// Unlock uszram.locks[lock_addr] as writer
 	return new_size;
 }
--- uszram.h
+++ uszram.h
@@ -9,15 +9,15 @@
  *
  * Logical block size, the smallest unit that can be read or written, is
  * (1 << USZRAM_BLOCK_SHIFT) bytes. USZRAM_BLOCK_SHIFT must be at least 0 and at
- * most 28, or at most 15 if your implementation uses 16-bit int.
+ * most 28 (or at most 14 if your implementation uses 16-bit int).
  *
- * USZRAM_BLOCK_ADDR_MAX is the maximum logical block address, or one fewer than
+ * USZRAM_BLK_ADDR_MAX is the maximum logical block address, or one fewer than
  * the number of logical blocks in the store. It must be at least 0 and at most
  * 2^32 - 1.
  *
  * Page size, the unit in which data is compressed, is (1 << USZRAM_PAGE_SHIFT)
- * bytes. USZRAM_PAGE_SHIFT must be at least USZRAM_BLOCK_SHIFT and at most 28,
- * or at most 15 if your implementation uses 16-bit int.
+ * bytes. USZRAM_PAGE_SHIFT must be at least USZRAM_BLOCK_SHIFT and at most 28
+ * (or at most 14 if your implementation uses 16-bit int).
  *
  * USZRAM_PG_PER_LOCK adjusts lock granularity for multithreading. It is the
  * maximum number of pages that can be controlled by a single readers-writer
@@ -33,10 +33,10 @@
 
 
 int uszram_init(void);
-int uszram_read_blk (int_least32_t blk_addr, char data[static USZRAM_BLOCK_SIZE]);
-int uszram_read_pg  (int_least32_t pg_addr,  char data[static USZRAM_PAGE_SIZE]);
-int uszram_write_blk(int_least32_t blk_addr, const char data[static USZRAM_BLOCK_SIZE]);
-int uszram_write_pg (int_least32_t pg_addr,  const char data[static USZRAM_PAGE_SIZE]);
+int uszram_read_blk (uint_least32_t blk_addr, char data[static USZRAM_BLOCK_SIZE]);
+int uszram_read_pg  (uint_least32_t pg_addr,  char data[static USZRAM_PAGE_SIZE]);
+int uszram_write_blk(uint_least32_t blk_addr, const char data[static USZRAM_BLOCK_SIZE]);
+int uszram_write_pg (uint_least32_t pg_addr,  const char data[static USZRAM_PAGE_SIZE]);
 
 
 #endif // USZRAM_H
