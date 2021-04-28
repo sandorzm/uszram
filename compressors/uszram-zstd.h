983e3cdd47e184c622f7f9923cfb81e9a94c56e9
Sandor Magyar <sandorzm@gmail.com>
2021-04-28 16:24:03 -0400

    Do partial decompression with Zstd

--- compressors/uszram-zstd.h
+++ compressors/uszram-zstd.h
@@ -18,9 +18,7 @@ static inline size_type compress(const char src[static USZRAM_PAGE_SIZE],
 static inline int decompress(struct page *pg, size_type bytes,
 			     char dest[static USZRAM_PAGE_SIZE])
 {
-	(void)bytes;
-	size_t ret = ZSTD_decompress(dest, USZRAM_PAGE_SIZE,
-				     pg->data, get_size(pg));
+	size_t ret = ZSTD_decompress(dest, bytes, pg->data, get_size(pg));
 	return ZSTD_isError(ret) ? -1 : 0;
 }
 
